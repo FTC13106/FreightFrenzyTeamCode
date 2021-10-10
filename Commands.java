9135e0433ebdb6a898a90d6104cfe14b3bcee23d
laupetre <laupetre@yahoo.com>
2021-10-10 09:02:38 -0400

    commit

---
+++ Commands.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Commands {
+
+   public void moveForward(){
+
+   }
+
+    public void moveBackward(){
+
+    }
+     public void rotateCounterClockwise(){
+
+     }
+    public void moveLeft(){
+
+    }
+    public void rotateClockwise(){
+
+    }
+    public void moveRight(){
+
+    }
+
+
+}
