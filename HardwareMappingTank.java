f04e1311b61314107a9173315e70209dd7bec34a
iceman <danheitzmann@gmail.com>
2021-11-07 09:49:39 -0500

    Update HardwareMappingTank.java
    Commenting out the motors and servos that are not currently on the robot

--- HardwareMappingTank.java
+++ HardwareMappingTank.java
@@ -39,36 +39,44 @@ public class HardwareMappingTank
         leftRearMotor = hwMap.get(DcMotor.class, "leftRearMotor");
         rightFrontMotor = hwMap.get(DcMotor.class, "rightFrontMotor");
         rightRearMotor = hwMap.get(DcMotor.class, "rightRearMotor");
-        carouselMotor = hwMap.get(DcMotor.class, "carouselMotor");
         webcamName = hwMap.get(WebcamName.class, "Webcam 1");
+        /*
+        carouselMotor = hwMap.get(DcMotor.class, "carouselMotor");
         elevatorMotor = hwMap.get(DcMotor.class, "elevatorMotor");
         intakeServo = hwMap.get(CRServo.class, "intakeServo");
         clawServo = hwMap.get(Servo.class, "clawServo");
+        */
 
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
         rightRearMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        /*
         carouselMotor.setDirection(DcMotor.Direction.FORWARD);
         elevatorMotor.setDirection(DcMotor.Direction.FORWARD);
+        */
 
         // Set all motors to zero power
         leftFrontMotor.setPower(0);
         leftRearMotor.setPower(0);
         rightFrontMotor.setPower(0);
         rightRearMotor.setPower(0);
+        /*
         carouselMotor.setPower(0);
         elevatorMotor.setPower(0);
         clawServo.setPosition(0);
         intakeServo.setPower(0);
+        */
+
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftRearMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightRearMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        /*
         elevatorMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        */
 
     }
 }
