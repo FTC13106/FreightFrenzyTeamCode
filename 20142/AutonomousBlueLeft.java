4d0eaa16d94a860b7e52c80c91f9a3e8fd722016
iceman <danheitzmann@gmail.com>
2021-11-28 09:11:58 -0500

    Updated Auton files

--- 20142/AutonomousBlueLeft.java
+++ 20142/AutonomousBlueLeft.java
@@ -15,34 +15,39 @@ public class AutonomousBlueLeft extends LinearOpMode {
          */
         commands.init(hardwareMap);
         objectDetection.init(hardwareMap);
+        telemetry.addData("webcam", "ready");
+        telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            // Read barcode, go to shipping hub, drop off preload, park in warehouse
+
+            // get barcode floor
+            telemetry.addData("Reading barcode", "...");
+            telemetry.update();
             int floor = objectDetection.getBarcodeFloor(objectDetection);
             float duckLocation = objectDetection.duckLocation;
             telemetry.addData("floor ", floor);
             telemetry.addData("duck location", duckLocation);
             telemetry.update();
 
-            // move off the wall 5
-            commands.moveForward(0.5,5,5);
+            // Come out of init, close claw
+            commands.closeClaw();
+            //get off the wall
+            commands.moveForward(0.5, 7, 8);
+            // move the elevator to correct position. floor1=3in,floor2=8.5in,floor3=14.75in,floor4=20.25
+            commands.elevatorMoveToFloor(floor,5);
             //face the hub
             commands.rotateClockwiseGyro(0.2,-30,8);
             //kiss the hub
             commands.moveForward(0.5,24,8);
-
-            // move the elevator to correct position. floor1=3in,floor2=8.5in,floor3=14.75in,floor4=20.25
-            commands.elevatorMoveToFloor(floor,5);
-
             //drop the box
             commands.openClaw();
-
             // clear the hub
             commands.moveBackward(0.5,5,3);
-
             //turn to face the warehouse
             commands.rotateCounterClockwiseGyro(0.2,90,10);
             //go to the warehouse
--- 20142/AutonomousBlueRight.java
+++ 20142/AutonomousBlueRight.java
@@ -15,27 +15,35 @@ public class AutonomousBlueRight extends LinearOpMode {
          */
         commands.init(hardwareMap);
         objectDetection.init(hardwareMap);
+        telemetry.addData("webcam", "ready");
+        telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            //read barcode, go to shipping hub, drop off preload, drive to carousel, park in storage unit
+
+            // get barcode floor
+            telemetry.addData("Reading barcode", "...");
+            telemetry.update();
             int floor = objectDetection.getBarcodeFloor(objectDetection);
             float duckLocation = objectDetection.duckLocation;
             telemetry.addData("floor ", floor);
             telemetry.addData("duck location", duckLocation);
             telemetry.update();
-            //go to shipping hub, drop off preload, drive to carosel, park in *warehouse?
+
+            // Come out of init, close claw
+            commands.closeClaw();
             //get off the wall
             commands.moveForward(0.5, 7, 8);
+            // move the elevator to correct position. floor1=3in,floor2=8.5in,floor3=14.75in,floor4=20.25
+            commands.elevatorMoveToFloor(floor,5);
             //face the shipping hub
             commands.rotateCounterClockwiseGyro(.2,30,10);
             //go to shipping hub
             commands.moveForward(0.5, 20, 8);
-
-            // move the elevator to correct position. floor1=3in,floor2=8.5in,floor3=14.75in,floor4=20.25
-            commands.elevatorMoveToFloor(floor,5);
 
             //drop preload box
             commands.openClaw();
